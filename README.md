54774c13e676e4f1569e23db865cde3ca1a55304
Daniel Kocsis <kocsis.daniel@gmail.com>
2019-04-12 19:25:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring reactive web demo application
+
+
+This is a small application meant to learn and experiment with Spring reactive web stack.
+
+## To run the application
+
+1. Run `./gradlew bootRun` from the terminal
+2. Open `http://localhost:8080` in your browser
---
+++ src/main/java/com/danielkocsis/spring/reactivewebdemo/ReactiveWebDemoApplication.java
@@ -0,0 +1,13 @@
+package com.danielkocsis.spring.reactivewebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebDemoApplication.class, args);
+	}
+
+}
